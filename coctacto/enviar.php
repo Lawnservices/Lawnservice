f66dfc2e4001bc45986db58160bc3ec918f7defc
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-03-28 17:31:16 -0400

    fix

--- coctacto/enviar.php
+++ coctacto/enviar.php
@@ -4,7 +4,7 @@ $name= $_POST["name"];
 $phone= $_POST["phone"];
 $message= $_POST["message"];
 
-$contenido = "name:". $name ."\nphone:".$phone . "\nmessage:" .$message;
+$contenido = "name:". $name . "\nphone:".$phone .  "\nmessage:" .$message;
 mail($destino,"contacto", $contenido);
 header("location:/index.html");
 ?>
