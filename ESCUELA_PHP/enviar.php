8b6ac7be6ff68fc7c2dd7f16518d8036f9748cdd
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-03-29 19:21:00 -0400

    fix

--- ESCUELA_PHP/enviar.php
+++ ESCUELA_PHP/enviar.php
@@ -1,11 +1,15 @@
 <?php
-$destino= "miguellaguna51@gmail.com";
-$Contacto="contact desde la web";
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$telefono = $_POST["telefono"];
+ 
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
 $mensaje = $_POST["mensaje"];
-$contenido = "nombre: " .$nombre . "\ncorreo: " . $correo . "\ntelefono:" .$telefono . "\nmensaje:" . $mensaje;
+
+$destino = "miguellaguna51@gmail.com";
+$Contacto = "contact desde la web";
+
+$contenido = "Nombre: " .$nombre . "\nCorreo: " . $correo . "\nTelefono:" .$telefono . "\nMensaje:". $mensaje;
+
 mail($destino,"Contacto", $contenido);
-header('Location:/index.html');
+header('Location:gracias.html');
 ?>
