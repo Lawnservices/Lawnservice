f091e78b6ce63b65c93e3b99b777b97c4b1f9570
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-03-30 13:50:32 -0400

    fix

--- ESCUELA_PHP/enviar.php
+++ ESCUELA_PHP/enviar.php
@@ -5,12 +5,13 @@ $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST["mensaje"];
 
-$destino = "mailto:LagunaLawnServices883@gmail.com?-subject=subject-text";
+$destino = "LagunaLawnServices883@gmail.com";
 $Contacto = "contact desde la web";
 
 $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono:" . $telefono . "\nMensaje:" . $mensaje;
 
 mail($destino, $correo, $telefono, $mensaje);
-header('Location:gracias.html');
+ echo "<script>alert('correo enviado exitosamente')</scrip>";
+ echo "<script>setTimeout(\"location.href=contacte.html'\",1000)</script>";
 ?>
  
